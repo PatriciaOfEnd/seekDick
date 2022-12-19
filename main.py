6f2a3ae864ad94e8aa5d43fdec0a35dfd5d8e21b
SEALchanPS <ParanoiaSeal@outlook.com>
2022-12-19 12:12:54 +0800

    Fix 解决了 input 两个变量时，可能因空格问题导致的赋值问题

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 """该模块为 seekDick 的主模块。
 """
 
-def seek_dick(name: str, gender: str):
+def seek_dick(name_: str, gender_: str):
     """该函数根据给定的 name 和 gender 变量，判定此对象是否有 Dick。
 
     Args:
-        name (str): 给定判定对象的姓名。
-        gender (str): 给定判定对象的性别。
+        name_ (str): 给定判定对象的姓名。
+        gender_ (str): 给定判定对象的性别。
     """
 
-    if name == "嘤嘤饭":
+    if name_ == "嘤嘤饭":
         return False
-    elif gender == "男性":
+    elif gender_ == "男性":
         return True
-    elif gender == "女性":
+    elif gender_ == "女性":
         return False
     else:
         return 2
 
 
 while True:
-    n, g = input("输入姓名和性别（男性/女性/其他性别），以空格隔开>").split()
-    if seek_dick(n, g):
-        print("hasDick")
-    elif not seek_dick(n, g):
-        print("noDick")
+    name = input("输入判定对象的姓名 >")
+    gender = input("输入判定对象的性别（男性/女性/其他性别）>")
+    if seek_dick(name, gender):
+        print(f"{name} hasDick")
+    elif not seek_dick(name, gender):
+        print(f"{name} noDick")
     else:
-        print("notSureHasOrNoDick")
+        print(f"{name} notSureHasOrNoDick")
